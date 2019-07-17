a51f66b2015859959d45d72afeb8647c2144e8dd
Mia Wong <miaw629@gmail.com>
2019-07-17 18:13:49 -0400

    attempt 3

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import SideBar from '../components/SideBar';
+import SideBar from '../components/SideBar/Index';
 
 const IndexPage = () => (
   <Layout>
