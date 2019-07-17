baa56f15558b6ca0ebebb6fbd0f1c0860a4653b3
Mia Wong <miaw629@gmail.com>
2019-07-17 18:06:56 -0400

    attempt 2

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from './node_modules/react';
+import React, { useState } from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import Nav from './Nav';
